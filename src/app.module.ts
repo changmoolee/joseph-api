578f3dd86466748a9799e13389b7733609b47968
Lee Changmoo <lcm8543@gmail.com>
2025-02-15 20:18:39 +0900

    Feat:[user] module 등 작성 및 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { User } from './user/user.entity'; // User 엔티티 추가
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), // 환경 변수 로드
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
