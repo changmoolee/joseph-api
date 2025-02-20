766b6916208995af6fd45966724bffc6794f9251
Lee Changmoo <lcm8543@gmail.com>
2025-02-20 19:07:45 +0900

    Fix:[회원가입 페이지] 회원가입 api - 1. 누락된 body 데코레이터 2. 응답값 및 dto 개선

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/auth/dto/create-user.dto';
 
@@ -7,7 +7,22 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post()
-  async signupUser(userDto: CreateUserDto) {
-    return this.authService.signupUser(userDto);
+  async signupUser(@Body() userDto: CreateUserDto) {
+    try {
+      await this.authService.signupUser(userDto);
+
+      return {
+        data: [],
+        result: 'success',
+        message: '회원가입을 성공하였습니다.',
+      };
+    } catch (error) {
+      console.error(error);
+      return {
+        data: [],
+        result: 'failure',
+        message: '회원가입을 실패하였습니다.',
+      };
+    }
   }
 }
