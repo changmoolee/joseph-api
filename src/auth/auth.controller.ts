1b286ac7913d7f2905e2c4204f38ed261fa03bd2
Lee Changmoo <lcm8543@gmail.com>
2025-02-26 15:48:22 +0900

    Refactor:[로그인] 로그인 api 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/auth/dto/create-user.dto';
 import { ApiResponseDto } from 'src/common/dto/response.dto';
+import { SigninUserDto } from 'src/auth/dto/signin-user.dto';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -12,5 +14,10 @@ export class AuthController {
     @Body() userDto: CreateUserDto,
   ): Promise<ApiResponseDto<[]>> {
     return this.authService.signupUser(userDto);
+  }
+
+  @Post('signin')
+  async signinUser(@Body() userDto: SigninUserDto, @Res() response: Response) {
+    this.authService.signinUser(userDto, response);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,13 @@
-import { Body, Injectable } from '@nestjs/common';
+import { Body, Res, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../user/user.entity';
 import * as bcrypt from 'bcrypt';
-import { CreateUserDto } from 'src/auth/dto/create-user.dto';
 import { ApiResponseDto } from 'src/common/dto/response.dto';
+import * as jose from 'jose';
+import { CreateUserDto } from 'src/auth/dto/create-user.dto';
+import { SigninUserDto } from 'src/auth/dto/signin-user.dto';
+import { Response } from 'express';
 
 @Injectable()
 export class AuthService {
@@ -50,6 +53,78 @@ export class AuthService {
         result: 'failure',
         message: '회원가입을 실패하였습니다.',
       };
+    }
+  }
+
+  async signinUser(@Body() userDto: SigninUserDto, @Res() response: Response) {
+    try {
+      // https://docs.nestjs.com/security/authentication
+      const findUser = await this.userRepository.findOne({
+        where: { email: userDto.email },
+      });
+
+      if (!findUser) {
+        response.json({
+          data: [],
+          result: 'failure',
+          message: '존재하지 않는 이메일입니다.',
+        });
+        return;
+      }
+
+      /** 패스워드 일치 여부 */
+      const isMatch = await bcrypt.compare(userDto.password, findUser.password);
+
+      // 불일치시 에러
+      if (!isMatch) {
+        response.json({
+          data: [],
+          result: 'failure',
+          message: '비밀번호가 일치하지 않습니다.',
+        });
+        return;
+      }
+
+      const JWT_SECRET = process.env.JWT_SECRET || '';
+
+      const alg = 'HS256';
+
+      const token = await new jose.SignJWT({
+        id: findUser.id,
+        email: findUser.email,
+      })
+        .setProtectedHeader({ alg })
+        .setIssuedAt()
+        .setExpirationTime('2h')
+        .sign(new TextEncoder().encode(JWT_SECRET));
+
+      response.cookie('token', token, {
+        httpOnly: true, // XSS 공격 방지
+        // secure: true, // HTTPS 환경에서만 쿠키 전송 (배포 환경에서는 true)
+        sameSite: 'strict', // CSRF 공격 방지
+        maxAge: 2 * 60 * 60 * 1000, // 2시간 후 만료
+      });
+
+      response.json({
+        data: {
+          id: findUser.id,
+          email: findUser.email,
+          image: findUser.image,
+          username: findUser.username,
+        },
+        result: 'success',
+        message: '로그인을 성공하였습니다.',
+      });
+      return;
+    } catch (error) {
+      console.error(error);
+
+      response.json({
+        data: [],
+        result: 'failure',
+        message: '',
+      });
+      return;
     }
   }
 }
