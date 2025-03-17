f9b6b684c39826d27d37ccfbf7e7e6ef42e7ebdf
Lee Changmoo <lcm8543@gmail.com>
2025-03-17 22:21:12 +0900

    Update:[useGlobalFilters] 글로벌 exception - 1. status, message 기본값 2. return 형식 이전과 동일하게 처리

--- src/common/filters/global-exception.filter.ts
+++ src/common/filters/global-exception.filter.ts
@@ -17,7 +17,20 @@ export class GlobalExceptionFilter implements ExceptionFilter {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
-    const status = exception.getStatus();
+
+    let status = 500;
+    let message = '예상치 못한 서버 오류가 발생했습니다.';
+
+    // NestJS의 기본 HTTP exception인지 확인 (ex) NotFoundException, BadRequestException)
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+
+      const exceptionResponse = exception.getResponse();
+      message =
+        typeof exceptionResponse === 'string'
+          ? exceptionResponse
+          : (exceptionResponse as any).message;
+    }
 
     // 에러 로깅
     this.logger.error(
@@ -25,9 +38,9 @@ export class GlobalExceptionFilter implements ExceptionFilter {
     );
 
     response.status(status).json({
-      statusCode: status,
-      timestamp: new Date().toISOString(),
-      path: request.url,
+      data: null,
+      result: 'failure',
+      message,
     });
   }
 }
