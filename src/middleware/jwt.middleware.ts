93a09e8902b26bdde83c1a03f47a5029cd1e02a9
Lee Changmoo <lcm8543@gmail.com>
2025-03-21 21:41:09 +0900

    Refactor:[로그인 인증] 기존 쿠키인증에서 토큰전달 방식으로 변경(iOS Safari 이슈)

--- src/middleware/jwt.middleware.ts
+++ src/middleware/jwt.middleware.ts
@@ -9,11 +9,15 @@ import * as jose from 'jose';
 @Injectable()
 export class JwtMiddleware implements NestMiddleware {
   async use(req: Request, res: Response, next: NextFunction) {
-    // 클라이언트 token 쿠키
-    const jwt = req.cookies.token;
+    // 클라이언트 token
+    const authorizationHeader = req.headers['authorization'] as string;
+
+    const jwt = authorizationHeader?.startsWith('Bearer ')
+      ? authorizationHeader.split(' ')[1]
+      : null;
 
     if (!jwt) {
-      throw new UnauthorizedException('쿠키가 존재하지 않습니다.');
+      throw new UnauthorizedException('토큰이 존재하지 않습니다.');
     }
 
     const JWT_SECRET = process.env.JWT_SECRET || '';
@@ -28,7 +32,7 @@ export class JwtMiddleware implements NestMiddleware {
       next();
     } catch (error) {
       console.error(error);
-      throw new UnauthorizedException('인증되지 않는 쿠키입니다.');
+      throw new UnauthorizedException('인증되지 않는 토큰입니다.');
     }
   }
 }
